d0842bb2858f433b376b57d9cb46a6e462e28add
diego <diegomotta18@gmail.com>
2020-05-25 12:58:20 -0300

    burger in react js

---
+++ src/containers/BurgerBuild/BurgerBuild.js
@@ -0,0 +1,27 @@
+import React,{ Component } from "react";
+import Aux from '../../hoc/Aux';
+import Burger from "../../components/Burguer/Burger";
+class BurgerBuilder extends Component{
+    // constructor (props){
+    //     super(props);
+    //     this.state = {}
+    // }
+    state = {
+        ingredients:{
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        }
+    }
+    render (){
+        return (
+                <Aux>
+                    <Burger  ingredients={this.state.ingredients}></Burger>
+                    <div>Builder tooll</div>
+                </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
