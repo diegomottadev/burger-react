cc1cf4059558c06107dd1490bb1bf6a6a1ec10e9
diego <diegomotta18@gmail.com>
2020-06-02 19:32:19 -0300

    change size logo for diferente screen

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,11 +4,14 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css'
 const sideDrawer = (props) =>{
     return (
-        <div>
-            <Logo/>
+        <div className={classes.sideDrawer}>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
             <nav>
                 <NavigationItems/>
             </nav>
         </div>
     );
 }
+export default sideDrawer;
