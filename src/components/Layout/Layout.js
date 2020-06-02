bb1c03ae9a8c5dc61407213c6160d8a959ea1ad7
diego <diegomotta18@gmail.com>
2020-06-02 20:04:37 -0300

    rezise responsive menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';    
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
+class Layout extends Component {
+    state ={
+        showSideDrawer: true
+    }
+    sideDrawerCloseHander = ()=>{
+        this.setState({showSideDrawer:false});
+    }
+    render(){
+        return(
+            <Aux>
+            <Toolbar/>
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHander}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+            </Aux>
+        );
 
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+    }
 
-);
 
-export default layout;
+}
+
+export default Layout;
